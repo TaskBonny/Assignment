336724895ad2ba838cb5c60d56fc9bd3fc8e08bb
TaskBonny <trkblc5@gmail.com>
2020-07-20 13:13:33 +0300

    FizzBuzz done

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+num[]
+for i in range(1,101):
+    num.append(i)
+for i in num:
+    if i%3==0 and i%5==0:
+        print("Fizz Buzz")
+    elif i%3==0:
+        print("Fizz")
+    elif i%5==0:
+        print("Buzz")
+    else:
+        print(i)
